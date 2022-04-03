f3e4a9c37bbc42c159f8c64865aefc650049f179
Aitor <98980628+terminaaitor05@users.noreply.github.com>
2022-04-04 00:25:12 +0200

    Add files via upload

---
+++ test/main.py
@@ -0,0 +1,11 @@
+import ppt
+import test
+
+def main():
+    input("Elige a qué quieres jugar: piedra, papel o tijera 'p', o adivina el número 'n': ")	
+    if input == 'p':
+        ppt.my_function()
+    else:
+        test.my_function()
+
+print(main())
