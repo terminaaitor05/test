301d52af4f9751f38169abf1adbf76db870492bd
Aitor <98980628+terminaaitor05@users.noreply.github.com>
2022-04-04 18:13:06 +0200

    Add files via upload

---
+++ ppt.py
@@ -0,0 +1,19 @@
+import random
+
+def play():
+    user = input("Elige piedra, 'r', papel, 'p', o tijera, 't': ")
+    computer = random.choice(['r', 'p', 't'])
+
+    if user == computer:
+        return 'Empate'
+    
+    if is_win(user, computer):
+        return 'Ganaste'
+
+    return 'Perdiste'
+
+def is_win(user, computer):
+    if (user == 'r' and computer == 'p') or (user == 'p' and computer == 't') or (user == 't' and computer == 'r'):
+        return True
+
+# print(play())
